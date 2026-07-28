a4d46f2055f2d4367db54178d90d83e325dcf733
Tshepiso Mathabatha <tshepisomathabatha4@gmail.com>
2026-07-28 12:40:39 +0200

    Forge: Additional Statistics Changes

--- statistics/customer statistics/customer statistical analyis.R
+++ statistics/customer statistics/customer statistical analyis.R
@@ -5,10 +5,12 @@ customers=read.csv('customers.csv',header = TRUE)
 setwd('C:/Users/tshep/mobile_carwash/data/analytics/customer_metrics')
 customer_lifetime_value=read.csv('customer_lifetime_value.csv', header = TRUE)
 
+length(customer_lifetime_value$total_amount_paid)
 
 #CUSTOMER INTELLIGENCE
 
-#descriptive statistics
+#SECTION1
+#DESCRIPTIVE STATISTIC
 #a. data summary
 ltv_summary=summary(customer_lifetime_value$total_amount_paid)
 ltv_summary
@@ -48,8 +50,14 @@ setwd('C:/Users/tshep/mobile_carwash/data/statistics/customer_metrics')
 #install.packages('kableExtra')
 
 
-#Confidence Interval
-#mean ltv CI
+
+
+
+
+#SECTION2
+#CONFIDENCE INTERVALS
+
+#mean ltv confidence interval
 average_ltv_ci=t.test(customer_lifetime_value$total_amount_paid, conf.level = 0.95)
 average_ltv_ci
 mean_ltv=average_ltv_ci$estimate
@@ -65,9 +73,27 @@ colnames(mean_ltv_ci)=c('mean', 'lower_bound', 'upper_bound')
 write.csv(mean_ltv_ci,'mean_ltv_ci.csv' )
 
 
+#repeat rate confidence interval
+setwd('C:/Users/tshep/mobile_carwash/data/analytics/customer_metrics')
+repeat_customers=read.csv('repeat_customers.csv', header = TRUE)
+customer_metrics=read.csv('customer_metrics.csv',header = TRUE)
 
+#Total customers
+total_customers=nrow(repeat_customers)
 
+#No. of customers with multiple_bookings
+multiple_bookings=repeat_customers[repeat_customers$booking_count>1,]
+repeat_customers_count=nrow(multiple_bookings)
 
+#ci
+#prop.test calculates the proportion on its own
+repeat_rate_ci=prop.test(repeat_customers_count,total_customers, conf.level = 0.95)
 
+repeat_rate=repeat_rate_ci$estimate
+rate_lower_ci=repeat_rate_ci$conf.int[1]
+rate_upper_ci=repeat_rate_ci$conf.int[2]
 
-
+repeat_rate_ci=data.frame(repeat_rate,rate_lower_ci,rate_upper_ci)
+colnames(repeat_rate_ci)=c('repeat_rate', 'lower_bound', 'upper_bound')
+setwd('C:/Users/tshep/mobile_carwash/data/statistics/customer_metrics')
+write.csv(repeat_rate_ci,'repeat_rate_ci.csv')
